f5ce851c3efefbee726730901dca213efe4878a8
Lonli-Lokli <lonli.lokli@gmail.com>
2025-05-29 16:46:18 +0100

    add light-medium roast

--- src/scoring.mjs
+++ src/scoring.mjs
@@ -336,11 +336,24 @@ export const roastLevelEffects = {
     abbreviation: "L",
     description: "Preserves origin character, bright and acidic",
     effects: {
-      sweetness: +0.5,
-      acidity: +1.5,
+      sweetness: 0.5,
+      acidity: 1.5,
       body: -1.0,
       balance: -0.5,
       bitterness: -1.0,
+    },
+  },
+  "light-medium": {
+    displayName: "Light-Medium Roast",
+    abbreviation: "LM",
+    description:
+      "A balanced roast level, between light and medium, offering a blend of origin acidity and developing body and sweetness.",
+    effects: {
+      sweetness: 0.75,
+      acidity: 0.25,
+      body: -0.25,
+      balance: 0.75,
+      bitterness: 0,
     },
   },
   medium: {
@@ -348,23 +361,24 @@ export const roastLevelEffects = {
     abbreviation: "M",
     description: "Balanced between origin and roast character",
     effects: {
-      sweetness: +0.75,
+      sweetness: 0.75,
       acidity: -0.75,
-      body: +0.75,
-      balance: +1.0,
-      bitterness: +0.75,
+      body: 0.75,
+      balance: 1.0,
+      bitterness: 0.75,
     },
   },
   "medium-dark": {
     displayName: "Medium-Dark Roast",
     abbreviation: "MD",
-    description: "Roast character begins to dominate origin flavors",
+    description:
+      "Roasted further to develop richer body and bittersweet notes, with less acidity.",
     effects: {
+      sweetness: 0.75,
       acidity: -1,
       body: 1,
-      sweetness: -0.5,
+      balance: 0.25,
       bitterness: 1,
-      balance: -0.5,
     },
   },
   dark: {
@@ -374,9 +388,9 @@ export const roastLevelEffects = {
     effects: {
       sweetness: -1.5,
       acidity: -3.0,
-      body: +0.5,
+      body: 0.5,
       balance: -1.5,
-      bitterness: +3.0,
+      bitterness: 3.0,
     },
   },
 };
