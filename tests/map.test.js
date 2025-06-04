9c030b5ee0c3d68aa20c89fc0147b8eea8637b07
Lonli-Lokli <lonli.lokli@gmail.com>
2025-06-04 22:15:29 +0100

    fixed tests

--- tests/map.test.js
+++ tests/map.test.js
@@ -4,7 +4,7 @@ import { expect, test } from "vitest";
 import { countries110m as world } from "../src/data/countries-110m";
 import { d3CountryNameNormilizer } from "../src/js/map.mjs";
 
-test.only("our coffee profiles are all available in d3 geo", () => {
+test("our coffee profiles are all available in d3 geo", () => {
   const coffeeCountries = Object.keys(coffeeProfiles);
   const d3Countries1 = topojson.feature(world, world.objects.countries);
 
